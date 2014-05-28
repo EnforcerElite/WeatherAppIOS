bb3e506303dc54b1330bbdbd483d0b61fb1659d8
Sinove <HDD@Sinove-MBP.local>
2014-05-28 18:19:01 +0200

    Initial Commit

---
+++ weatherApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  weatherApp
+//
+//  Created by Sinove on 28/05/14.
+//  Copyright (c) 2014 Sinove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ weatherApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  weatherApp
+//
+//  Created by Sinove on 28/05/14.
+//  Copyright (c) 2014 Sinove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
